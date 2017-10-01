68db4c43d2fc9802c6fc1458ee2d401742dcdcd2
George Codat Tudoran <office@solcreation.ro>
2017-10-01 20:55:20 +0300

    get weather forecast using 3rd party api

--- emailer.py
+++ emailer.py
@@ -1,3 +1,4 @@
+import requests
 
 def get_emails():
     emails = {}
@@ -25,11 +26,27 @@ def get_schedule():
 
     return schedule
 
+def get_weather_forecast():
+    url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ace81c554edf6297e213326bfe5ed2ac'
+    weather_request = requests.get(url)
+    weather_json = weather_request.json()
+
+    print(weather_json)
+
+    description = weather_json['weather'][0]['description']
+    print(description)
+    temp_min = weather_json['main']['temp_min']
+    temp_max = weather_json['main']['temp_max']
+    print(temp_min)
+    print(temp_max)
+
 def main():
     emails = get_emails()
     print(emails)
 
     schedule = get_schedule()
     print(schedule)
+
+    get_weather_forecast()
 
 main()
